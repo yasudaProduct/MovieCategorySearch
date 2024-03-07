ffc55a6ff2ca7981e8b306b6b8f949e2e10173a3
yuta <>
2024-03-08 00:20:39 +0900

    テーブル定義 deleted_flgをデフォルト"0"に変更

--- MovieCategorySearch.Infrastructure/Data/PostgresDbContext.cs
+++ MovieCategorySearch.Infrastructure/Data/PostgresDbContext.cs
@@ -8,6 +8,7 @@ public class PostgresDbContext : DbContext
     public PostgresDbContext(DbContextOptions<PostgresDbContext> options)
         : base(options)
     {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
     }
 
     public DbSet<User> User { get; set; } = default!;
@@ -16,24 +17,31 @@ public class PostgresDbContext : DbContext
 
     public DbSet<Category> Category { get; set; } = default!;
 
-    public DbSet<CategoryMap> CCategoryMap { get; set; } = default!;
+    public DbSet<CategoryMap> CategoryMap { get; set; } = default!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>()
+            .Property(u => u.DeletedFlg).HasDefaultValue("0");
         modelBuilder.Entity<User>()
             .HasMany(u => u.Categorys)
             .WithOne(c => c.User)
             .HasForeignKey(c => c.CreateUserId);
 
+
+        modelBuilder.Entity<Movie>()
+            .Property(m => m.DeletedFlg).HasDefaultValue("0");
         modelBuilder.Entity<Movie>()
             .HasMany(m => m.CategoryMaps)
             .WithOne(c => c.Movie)
             .HasForeignKey(c => c.MovieId);
 
         modelBuilder.Entity<Category>()
-        .HasMany(c => c.CategoryMaps)
-        .WithOne(cm => cm.Category)
-        .HasForeignKey(cm => cm.CategoryId);
+            .Property(c => c.DeletedFlg).HasDefaultValue("0");
+        modelBuilder.Entity<Category>()
+            .HasMany(c => c.CategoryMaps)
+            .WithOne(cm => cm.Category)
+            .HasForeignKey(cm => cm.CategoryId);
     }
 
 }
