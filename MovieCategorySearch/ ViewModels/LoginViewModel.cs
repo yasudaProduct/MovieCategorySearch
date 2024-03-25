4eaeebf70b43c9fea33d8010defbd767893014b7
yuta <yasuda_yuuta@intec-sp.co.jp>
2024-03-25 15:52:08 +0900

    messeage

--- MovieCategorySearch/ ViewModels/LoginViewModel.cs
+++ MovieCategorySearch/ ViewModels/LoginViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using Merino.Attribute;
+using MovieCategorySearch.Resources;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MovieCategorySearch.ViewModels
@@ -6,13 +8,14 @@ namespace MovieCategorySearch.ViewModels
     public class LoginViewModel
     {
         [DisplayName("メールアドレス")]
-        [Required]
-        [EmailAddress]
+        [MerinoRequired]
+        //[EmailAddress(ErrorMessageResourceName = nameof(UIMessageResource.Validator_EmailAddressAttribute),ErrorMessageResourceType = typeof(UIMessageResource))]
+        [MerinoEmailAddress(nameof(ValidateMessageResource.Validator_EmailAddressAttribute))]
         public string LoginId { get; set; }
 
         [DisplayName("パスワード")]
         [DataType(DataType.Password)]
-        [Required]
+        [MerinoRequired(nameof(ValidateMessageResource.MS0001))]
         public string Password { get; set; }
     }
 }
