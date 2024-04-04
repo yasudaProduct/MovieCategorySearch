173ed842cfa42690b1a3f8dde99215de655e4bcb
yasudaProduct <yasu.private.ct@gmail.com>
2024-04-05 01:41:45 +0900

    検証に引っかかったので

--- MovieCategorySearch/Models/BaseViewModel.cs
+++ MovieCategorySearch/Models/BaseViewModel.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace MovieCategorySearch.Models
 {
     public class BaseViewModel
     {
+        [ValidateNever]
         public string Messenge { get; set; } = "基底モデルからのメッセージです。";
 
+        [ValidateNever]
         public string PageScript { get; set; }
 
         public string GetViewIdentifier()
